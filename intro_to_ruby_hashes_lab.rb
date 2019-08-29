6cb45b24a47b6eeb989d13f7a4611f6284df1136
Adeja Lendor <alendor6@gmail.com>
2019-08-29 20:56:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,7 @@ def monopoly_with_third_tier
                    :three_pieces_owned => 100, 
                    :four_pieces_owned => 200},
                    :names => 
-                   {:reading_railroad => mortgage_value, 
+                   {:reading_railroad => :mortgage_value, 
                    :pennsylvania_railroad => {},
                    :b_and_o_railroad => {},
                    :shortline_railroad => {}}}}
